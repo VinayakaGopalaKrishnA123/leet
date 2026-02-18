e15cd994fc5c2d26d6d15061576d24114fb122b6
Chavali J S R Vinayaka Gopala Krishna <147472515+VinayakaGopalaKrishnA123@users.noreply.github.com>
2026-02-18 14:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.43%) - LeetSync

---
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev=n%2;
+        n/=2;
+        while(n>0){
+            if(prev==n%2) return false;
+            prev=n%2;
+            n/=2;
+        }
+        return true;
+    }
+};
